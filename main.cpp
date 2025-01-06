705fd1c8462a7bd01dbd4df42d7ef869ea3e5f9b
Schridzerkyphaladzovtrexoffeqxovaxeimesabakschaffrondovaxxjavvromazzanoataymaeinmzzzmuryjythnittzaqqleinzofffffmavzzppplkm <101307566+Renzwsafk@users.noreply.github.com>
2025-01-06 05:29:09 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+const int screenWidth = 1024;
+const int screenHeight = 768;
+const char* windowName = "Stumppadeth";
+
+int main(int argc, char* argv[])
+{
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, windowName, NULL, NULL);
+
+    if (window == NULL) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
